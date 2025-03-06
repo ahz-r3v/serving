1de129e492b98b2a18bf1f48837ae2b27a39e078
ahz-r3v <2521920975@qq.com>
2025-03-07 00:57:08 +0800

    using k8s load banlancing

--- cmd/autoscaler/main.go
+++ cmd/autoscaler/main.go
@@ -145,7 +145,11 @@ func main() {
 		statsScraperFactoryFunc(podLister, networkConfig.EnableMeshPodAddressability, networkConfig.MeshCompatibilityMode), logger)
 
 	// Set up scalers.
-	conn, err := grpc.Dial("scale-predictor-service:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(
+		"dns:///scale-predictor-service.knative-serving.svc.cluster.local:50051", 
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
+	)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
